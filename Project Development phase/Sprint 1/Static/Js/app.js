6cf3ac86177beb2fcbc8cc4423dd08ee48343875
sawdeshwaran <98637478+sawdeshwaran@users.noreply.github.com>
2022-11-23 11:20:39 +0530

    Update app.js

--- Project Development phase/Sprint 1/Static/Js/app.js
+++ Project Development phase/Sprint 1/Static/Js/app.js
@@ -1,3 +1,8 @@
-
+const selectedItem = document
+  .getElementById('sidebarCollapse')
+  .addEventListener('click', (e) => {
+    const ele = document.getElementById('sidebar');
+    ele.classList.toggle('active');
+  });
 
 
